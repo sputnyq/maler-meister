2b39c1722b19d0bfe044b4c63bdf93fdf469657f
Nikolai Kabiolskij <12097812+mee3003@users.noreply.github.com>
2023-06-29 22:11:33 +0200

    removed intl

--- src/main.tsx
+++ src/main.tsx
@@ -4,31 +4,13 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import { IntlProvider } from "react-intl";
-
-import de from "./translations/de.json";
-import ru from "./translations/ru.json";
-
-const messages = {
-  de,
-  ru,
-};
-type SupportedLanguages = "de" | "ru";
-
-const language = navigator.language.split(/[-_]/)[0] as SupportedLanguages;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <IntlProvider
-      locale={navigator.language}
-      defaultLocale="de"
-      messages={messages[language]}
-    >
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    </IntlProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );

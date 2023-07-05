7104570aec824745572a68c45becfc83809b721e
Nikolai Kabiolskij <12097812+mee3003@users.noreply.github.com>
2023-07-05 17:21:37 +0200

    icons hinzugefügt

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
 
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ manifest: { name: "Maler Meister", short_name: "MM" } }),
+    VitePWA({
+      manifest: {
+        name: 'Maler Meister',
+        short_name: 'MM',
+      },
+    }),
   ],
 });

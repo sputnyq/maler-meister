edaf1aadcc612c4eea18b43934d26f6c5e97e49a
Nikolai Kabiolskij <12097812+mee3003@users.noreply.github.com>
2023-07-13 18:07:34 +0200

    angelegt

---
+++ src/features/my-vacations/index.tsx
@@ -0,0 +1,132 @@
+import BeachAccessIcon from '@mui/icons-material/BeachAccessOutlined';
+import {
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ToggleButton,
+  ToggleButtonGroup,
+  Typography,
+} from '@mui/material';
+
+import React, { useEffect, useState } from 'react';
+
+import { loadDailyEntries } from '../../fetch/api';
+import { useCurrentUser } from '../../hooks/useCurrentUser';
+
+import { addYears, endOfYear, startOfYear } from 'date-fns';
+
+export default function MyVacations() {
+  const [yearSwitchValue, setYearSwitchValue] = useState<'last' | 'current' | 'next'>('current');
+  const user = useCurrentUser();
+
+  const [data, setData] = useState<DailyEntry[]>([]);
+
+  useEffect(() => {
+    let dateObj = undefined;
+
+    const now = new Date();
+
+    switch (yearSwitchValue) {
+      case 'current':
+        dateObj = {
+          $gte: startOfYear(now),
+          $lt: now,
+        };
+        break;
+
+      case 'last':
+        dateObj = {
+          $gte: startOfYear(addYears(now, -1)),
+          $lt: endOfYear(addYears(now, -1)),
+        };
+        break;
+      case 'next':
+        dateObj = {
+          $gt: now,
+        };
+    }
+    const queryObj = {
+      filters: {
+        username: user?.username,
+        type: 'Urlaub',
+        date: dateObj,
+      },
+      sort: { '0': 'date:desc' },
+    };
+
+    loadDailyEntries(queryObj).then((data) => {
+      if (data) {
+        setData(data);
+      }
+    });
+  }, [yearSwitchValue, user]);
+
+  return (
+    <Card>
+      <CardHeader title="Mein Urlaub" />
+      <CardContent>
+        <Box display={'flex'} flexDirection="column" gap={2}>
+          <Box maxWidth={400}>
+            <ToggleButtonGroup
+              color="primary"
+              fullWidth
+              exclusive
+              value={yearSwitchValue}
+              onChange={(_, value) => {
+                value && setYearSwitchValue(value);
+              }}
+            >
+              <ToggleButton size="small" value="last">
+                Letztes Jahr
+              </ToggleButton>
+              <ToggleButton size="small" value="current">
+                Laufendes Jahr
+              </ToggleButton>
+              <ToggleButton size="small" value="next">
+                Zukunft
+              </ToggleButton>
+            </ToggleButtonGroup>
+          </Box>
+
+          <Box display={'flex'} justifyContent="space-between">
+            <Typography color={'GrayText'} variant="h6">
+              Gesamt:
+            </Typography>
+            <Typography color={'GrayText'} variant="h6">{`${data.length} Tag(e)`}</Typography>
+          </Box>
+
+          <Box>
+            <List>
+              {data.map((de) => {
+                return (
+                  <React.Fragment key={de.id}>
+                    <ListItem>
+                      <ListItemIcon>
+                        <BeachAccessIcon color="warning" />
+                      </ListItemIcon>
+                      <ListItemText>
+                        {new Intl.DateTimeFormat('de-DE', {
+                          month: 'long',
+                          weekday: 'long',
+                          day: 'numeric',
+                          year: 'numeric',
+                        }).format(new Date(de.date))}
+                      </ListItemText>
+                    </ListItem>
+                    <Divider />
+                  </React.Fragment>
+                );
+              })}
+            </List>
+          </Box>
+        </Box>
+      </CardContent>
+    </Card>
+  );
+}

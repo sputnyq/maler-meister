6dfea1f154319802dce51171f2243baf4e81127f
Nikolai Kabiolskij <12097812+mee3003@users.noreply.github.com>
2023-07-13 12:42:55 +0200

    fix

--- src/components/RequestDailyViewButton.tsx
+++ src/components/RequestDailyViewButton.tsx
@@ -7,8 +7,6 @@ interface Props {
 }
 
 export function RequestDailyViewButton({ value, onClick }: Props) {
-  const = useInt
-  
   return (
     <Button color="info" startIcon={<OpenInNewOutlinedIcon />} onClick={onClick}>
       {new Intl.DateTimeFormat('de-DE', {

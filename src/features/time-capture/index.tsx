cee1d8800ce1da508837c58aae59993d29b566d9
Nikolai Kabiolskij <12097812+mee3003@users.noreply.github.com>
2023-07-14 12:13:23 +0200

    rename routes and components

--- src/features/time-capture/index.tsx
+++ src/features/time-capture/index.tsx
@@ -2,7 +2,7 @@ import { Card, CardContent, CardHeader } from '@mui/material';
 
 import { useState } from 'react';
 
-import { UserTimes } from './MyTimes';
+import { MyTimes } from './MyTimes';
 import { TimeCaptureFlow } from './TimeCaptureFlow';
 
 export default function TimeCapture() {
@@ -12,7 +12,7 @@ export default function TimeCapture() {
     <Card>
       <CardHeader title="Meine Zeiten" />
       <CardContent>
-        <UserTimes update={update} requestUpdate={() => setUpdate((u) => u + 1)} />
+        <MyTimes update={update} requestUpdate={() => setUpdate((u) => u + 1)} />
       </CardContent>
       <TimeCaptureFlow requestUpdate={() => setUpdate((u) => u + 1)} />
     </Card>

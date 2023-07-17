d9a200ab9b0cec14d5b2dbc90086183be69ddb94
Nikolai Kabiolskij <12097812+mee3003@users.noreply.github.com>
2023-07-17 18:13:57 +0200

    print settings implemented

---
+++ src/features/options/print-settings/PrintSettingsEdit.tsx
@@ -0,0 +1,70 @@
+import { Box, Button, Grid, Typography } from '@mui/material';
+
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import AppGrid from '../../../components/AppGrid';
+import { AppDispatch } from '../../../store';
+import { updatePrintSettings } from '../../../store/printSettingsReducer';
+import { PSField } from './PSField';
+
+interface Props {
+  ps: PrintSettingsRoot;
+}
+
+export function PrintSettingsEdit({ ps }: Props) {
+  const [settings, setSettings] = useState(ps.settings || {});
+
+  const dispatch = useDispatch<AppDispatch>();
+  const Field = (prop: keyof PrintSettings) => {
+    return <PSField prop={prop} settings={settings} setSettings={setSettings} />;
+  };
+
+  const handleSave = () => {
+    dispatch(updatePrintSettings({ ...ps, settings }));
+  };
+
+  return (
+    <AppGrid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Box display={'flex'} flexDirection="column" gap={1}>
+          <Typography variant="h5" align="right">
+            Über
+          </Typography>
+          {[Field('companyName'), Field('ownerName'), Field('taxNumber')]}
+        </Box>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Box display={'flex'} flexDirection="column" gap={1}>
+          <Typography variant="h5" align="right">
+            Bank
+          </Typography>
+          {[Field('bank'), Field('iban'), Field('bic')]}
+        </Box>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Box display={'flex'} flexDirection="column" gap={1}>
+          <Typography variant="h5" align="right">
+            Kontakt
+          </Typography>
+          {[Field('phone'), Field('mobile'), Field('fax'), Field('email'), Field('web')]}
+        </Box>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Box display={'flex'} flexDirection="column" gap={1}>
+          <Typography variant="h5" align="right">
+            Adresse
+          </Typography>
+          {[Field('addressStreet'), Field('addressNumber'), Field('addressZip'), Field('addressCity')]}
+        </Box>
+      </Grid>
+      <Grid item xs={12}>
+        <Box display={'flex'} justifyContent="end">
+          <Button variant="contained" disableElevation onClick={handleSave}>
+            Speichern
+          </Button>
+        </Box>
+      </Grid>
+    </AppGrid>
+  );
+}
--- src/features/options/print-settings/index.tsx
+++ src/features/options/print-settings/index.tsx
@@ -1,5 +1,37 @@
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Accordion, AccordionDetails, AccordionSummary, Box } from '@mui/material';
+
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { AppState } from '../../../store';
+import { CreatePrintSetting } from './CreatePrintSetting';
+import { PrintSettingsEdit } from './PrintSettingsEdit';
 
 export default function PrintSettings() {
-  return <div>PrintSettings</div>;
+  const printSettings = useSelector<AppState, PrintSettingsRoot[]>((s) => s.prinSettings.all || []);
+
+  return (
+    <>
+      <Box>
+        {printSettings.map((ps) => {
+          return (
+            <Accordion>
+              <AccordionSummary
+                aria-controls={`${ps.name}-content`}
+                id={`${ps.name}-header`}
+                expandIcon={<ExpandMoreIcon />}
+              >
+                {ps.name}
+              </AccordionSummary>
+              <AccordionDetails>
+                <PrintSettingsEdit ps={ps} />
+              </AccordionDetails>
+            </Accordion>
+          );
+        })}
+      </Box>
+      <CreatePrintSetting />
+    </>
+  );
 }

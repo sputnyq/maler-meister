df080d3acb988c5353529122f70c9487faec011f
Nikolai Kabiolskij <12097812+mee3003@users.noreply.github.com>
2023-07-25 14:22:47 +0200

    save and edit works

--- src/store/offerReducer.ts
+++ src/store/offerReducer.ts
@@ -1,3 +1,4 @@
+import { AppState } from '.';
 import { offerById } from '../fetch/endpoints';
 import { appRequest } from '../fetch/fetch-client';
 import { genericConverter } from '../utilities';
@@ -15,26 +16,52 @@ const initialState: RootState = {
   unsavedChanges: false,
 };
 
-const createOffer = () => {
+const initializeOffer = () => {
   const offer = {
-    offerServices: new Array<BgbOfferService>(),
+    offerServices: [{} as BgbOfferService],
+
     text: '',
   } as AppOffer;
   return offer;
 };
 
-export const loadOffer = createAsyncThunk('Offers/load', async (id: string | number) => {
+export const loadOffer = createAsyncThunk('offers/load', async (id: string | number) => {
   const response = await appRequest('get')(offerById(id));
 
   return genericConverter<AppOffer>(response.data);
 });
+
+export const updateOffer = createAsyncThunk<AppOffer, void, { state: AppState }>(
+  'offers/update',
+  async (_, thunkApi) => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const offer = thunkApi.getState().offer.current!; // never happens
+
+    const response = await appRequest('put')(offerById(offer.id), { data: offer });
+
+    return genericConverter<AppOffer>(response.data);
+  },
+);
+
+export const createOffer = createAsyncThunk<AppOffer, { cb: (id: string | number) => void }, { state: AppState }>(
+  'offers/create',
+  async (payload, thunkApi) => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const offer = thunkApi.getState().offer.current!; // never happens
+
+    const response = await appRequest('post')(offerById(''), { data: offer });
+    const converted = genericConverter<AppOffer>(response.data);
+    payload.cb(converted.id);
+    return converted;
+  },
+);
 
 const offerSlice = createSlice({
   name: 'offer',
   initialState,
   reducers: {
     initOffer(state) {
-      state.current = createOffer();
+      state.current = initializeOffer();
     },
     setOfferProp(state, action: PayloadAction<{ path: string[]; value: any }>) {
       const { path, value } = action.payload;
@@ -47,10 +74,19 @@ const offerSlice = createSlice({
     },
   },
   extraReducers(builder) {
-    builder.addCase(loadOffer.fulfilled, (state, action) => {
-      state.current = action.payload;
-      state.unsavedChanges = false;
-    });
+    builder
+      .addCase(loadOffer.fulfilled, (state, action) => {
+        state.current = action.payload;
+        state.unsavedChanges = false;
+      })
+      .addCase(updateOffer.fulfilled, (state, action) => {
+        state.current = action.payload;
+        state.unsavedChanges = false;
+      })
+      .addCase(createOffer.fulfilled, (state, action) => {
+        state.current = action.payload;
+        state.unsavedChanges = false;
+      });
   },
 });
 

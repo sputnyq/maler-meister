3c0764ea7fd136a7ef525476e371d130129d5d62
Nikolai Kabiolskij <12097812+mee3003@users.noreply.github.com>
2023-07-31 10:43:59 +0200

    organize imports

--- src/features/options/print-settings/PrintSettingsEdit.tsx
+++ src/features/options/print-settings/PrintSettingsEdit.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Card, CardContent, CardHeader, Grid, Typography } from '@mui/material';
+import { Box, Button, Card, CardContent, CardHeader } from '@mui/material';
 
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
